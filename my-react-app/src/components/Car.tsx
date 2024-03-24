ff4ddfb37cb8444b3ef7f906e3a29b2bb4f2172a
thisasta <abishan.aranke@outlook.de>
2024-03-24 16:22:42 +0100

    card component added

--- my-react-app/src/components/Car.tsx
+++ my-react-app/src/components/Car.tsx
@@ -1,12 +1,66 @@
+import Card from "./Card.tsx";
+import porsche from "../assets/porsche-model.png";
+import bmw from "../assets/bmw-model.png";
+import audi from "../assets/audi-model.webp"
+
 function Car() {
     const car1 = "Porsche";
-    const car2 = "bmw";
+    const car2 = "BMW";
+    const car3 = "Audi";
 
     return(
-        <ul className="font-poppins">
-            <li>{car1}</li>
-            <li>{car2.toUpperCase()}</li>
-        </ul>
+        <div className="flex-col font-poppins bg-gray-800 text-white p-5 h-dvh">
+            <ul className="flex gap-20 my-10 justify-around">
+                <li>
+                    <Card title={car1} url="https://www.porsche.com/" picture={porsche}>
+                        Best Car which was ever built.
+                    </Card>
+                </li>
+                <li>
+                    <Card title={car2} url="https://www.bmw.de/" picture={bmw}>
+                        It is Porsche but for less money.
+                    </Card>
+                </li>
+                <li>
+                    <Card title={car3} picture={audi} url={"https://www.audi.de/de/brand/de.html"}>
+                        Drivers of that car tend to think they are special just because all four of their wheels are
+                        connected to the engine.
+                    </Card>
+                </li>
+                <li>
+                    <Card title={car3} picture={audi} url={"https://www.audi.de/de/brand/de.html"}>
+                        Drivers of that car tend to think they are special just because all four of their wheels are
+                        connected to the engine.
+                    </Card>
+                </li>
+            </ul>
+            <ul className="flex gap-20 justify-around">
+                <li>
+                    <Card title={car3} picture={audi} url={"https://www.audi.de/de/brand/de.html"}>
+                        Drivers of that car tend to think they are special just because all four of their wheels are
+                        connected to the engine.
+                    </Card>
+                </li>
+                <li>
+                    <Card title={car3} picture={audi} url={"https://www.audi.de/de/brand/de.html"}>
+                        Drivers of that car tend to think they are special just because all four of their wheels are
+                        connected to the engine.
+                    </Card>
+                </li>
+                <li>
+                    <Card title={car3} picture={audi} url={"https://www.audi.de/de/brand/de.html"}>
+                        Drivers of that car tend to think they are special just because all four of their wheels are
+                        connected to the engine.
+                    </Card>
+                </li>
+                <li>
+                    <Card title={car3} picture={audi} url={"https://www.audi.de/de/brand/de.html"}>
+                        Drivers of that car tend to think they are special just because all four of their wheels are
+                        connected to the engine.
+                    </Card>
+                </li>
+            </ul>
+        </div>
     );
 }
 
